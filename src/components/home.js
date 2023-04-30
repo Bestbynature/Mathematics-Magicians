03dffb416a0ecf4a0f56c4136f608d575c785ebf
Dammy <bestbynature2010@gmail.com>
2023-04-30 15:21:03 +0100

    :rocket: Add home component

---
+++ src/components/home.js
@@ -0,0 +1,17 @@
+const Home = () => (
+  <div className="home">
+    <h2>Welcome to our page!</h2>
+    <p>
+      <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit animi culpa  </span>
+      <span> nemo, distinctio magni autem reiciendis odio id nihil, voluptatibus recusandae  </span>
+    </p>
+    <p>
+      <span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis nesciunt</span>
+      <span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis nesciunt</span>
+      <span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis nesciunt</span>
+    </p>
+
+  </div>
+);
+
+export default Home;
